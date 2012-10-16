d24437ecf4048f226f8ce50389eb02a365866e1e
Tom Wilson <tom@jackhq.com>
2012-10-16 17:03:00 -0400

    remove foo dir

--- foo/app/scripts/app.js
+++
@@ -1,6 +0,0 @@
-var app = angular.module('foo', [])
-  .config(function($routeProvider){
-    $routeProvider
-      .when('/', { controller: 'Index', templateUrl: '/app/views/index.html' });
-  });
-  
--- foo/app/scripts/controllers/index.js
+++
@@ -1,3 +0,0 @@
-app.controller('Index', function($scope) {
-  
-});
